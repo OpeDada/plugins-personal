6b92e95298d1a24137f02e9264ce0e01594cff0c
OpeDada <opedada@gmail.com>
2024-03-28 16:24:40 +0900

    implement autocomplete feature

--- location-name-widget.js
+++ location-name-widget.js
@@ -2,7 +2,7 @@ const html = `
   <style>
     html, body {
       margin: 0;
-      width: 100%;
+      width: 350px;
     }
     #wrapper {
       padding: 18px;
@@ -15,30 +15,82 @@ const html = `
       background-size: 200% 100%;
     }
     input {
-      width: 97%;
+      width: 95%;
       border-radius: 6px;
-      margin-bottom: 10px;
-      padding: 4px;
       border: 1px solid #E91E63
+      margin-bottom: 10px;
+      padding: 4px 0;
+    }
+    .suggestions {
+      overflow-y: auto; /* Enable vertical scrolling when content exceeds the height */
+      color: #FFFFFF;
+      cursor: pointer;
+      padding: 4px;
+    }
+    .suggestion-item {
+      padding:10px;
+      cursor: pointer;
+    }
+    .suggestion-item:hover {
+      border-radius: 6px;
+      background-color: #01494B
     }
     #fly {
       cursor: pointer;
       border-radius: 6px;
       padding: 2px 12px;
+      margin-top: 8px;
       border: 1px solid #E91E63
     }
   </style>
 
   <div id="wrapper">
-    <input type="text" id="address" placeholder="Enter address/name of place">
+    <input type="text" id="address" autocomplete="off" placeholder="Enter address/name of place">
+    <div class="suggestions" id="suggestions"></div>
     <button id="fly">Locate</button>
   </div>
 
   <script>
+    //get the suggestions div
+    const suggestions = document.getElementById("suggestions");
+
     // Function to get input address
     const getAddress = () => ({
       address: document.getElementById("address").value.trim()
     });
+
+    // Event listener for input changes
+    // fnc responsible for handling the input event and making async requests to fetch location data.
+    document.getElementById("address").addEventListener("input", async () => {
+      const { address } = getAddress();
+      suggestions.innerHTML = ""; // Clear previous suggestions
+      if (address) {
+        try {
+          // fetch location data based on user input
+          const response = await fetch('https://nominatim.openstreetmap.org/search?format=json&q=' + encodeURIComponent(address))
+          const data = await response.json();
+          if (data && data.length > 0){
+            data.slice(0, 5).forEach(location => {
+              const item = document.createElement('div');
+              item.classList.add('suggestion-item');
+              item.textContent = location.display_name;
+              item.addEventListener('click', () => {
+                document.getElementById("address").value = location.display_name;
+                suggestions.innerHTML = '';
+              });
+              suggestions.appendChild(item);
+            });
+            suggestions.style.display = 'block'; // Show suggestions if there is a location
+          } else {
+            suggestions.style.display = 'none'; // Hide suggestions if there is no location
+          }
+        } catch (error) {
+          console.error('Error fetching location:', error);
+        }
+      } else {
+        suggestions.style.display = 'none'; // Hide suggestions if input is empty
+      }
+    })
 
     // Event listener for "Fly to" button
     document.getElementById("fly").addEventListener("click", async () => {
